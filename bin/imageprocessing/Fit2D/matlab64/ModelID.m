ea0383b9b01e09f13a9e168d9caa78fd6309985b
Eric <n.daguan@gmail.com>
2021-01-03 21:33:55 -0500

    add 2d guass gpu-fit package

---
+++ bin/imageprocessing/Fit2D/matlab64/ModelID.m
@@ -0,0 +1,15 @@
+classdef ModelID
+    properties (Constant = true)
+        GAUSS_1D = 0
+        GAUSS_2D = 1
+        GAUSS_2D_ELLIPTIC = 2
+        GAUSS_2D_ROTATED = 3
+        CAUCHY_2D_ELLIPTIC = 4
+        LINEAR_1D = 5
+        FLETCHER_POWELL = 6
+        BROWN_DENNIS = 7
+		GAUSS_2D_x2 = 8
+		GAUSS_2D_x3 = 9
+		GAUSS_2D_x4 = 10
+    end
+end
